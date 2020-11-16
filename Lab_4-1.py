ece964dae44802f755637e754d1cbc08c7bb97a1
Francesco <p21fthomas@fairfieldprep.org>
2020-11-16 13:25:00 -0500

    Lab_4-1

---
+++ Lab_4-1.py
@@ -0,0 +1,14 @@
+yeah_colors = ["Blue","Yellow","Green","Red"]
+
+yeah_colors.extend(["Orange","Yellow","Purple"])
+print(yeah_colors)
+
+print(yeah_colors.count("Yellow"))
+
+yeah_colors.insert(3, 'Red')
+print(yeah_colors)
+
+yeah_colors.clear()
+print(yeah_colors)
+
+print(yeah_colors.count('Red'))
